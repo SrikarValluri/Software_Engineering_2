5ba537c81db9a0a2673cde3e06ad74bc078ef42e
SrikarValluri <valluris@oregonstate.edu>
2021-03-07 23:29:10 -0800

    q2 sucessful test3 cases

--- hw7/q2_test.py
+++ hw7/q2_test.py
@@ -5,7 +5,9 @@ class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(leapyear(2000), "Leap Year")
     def test2(self):
-        self.assertEqual(leapyear(100), "Not Leap Year")
+        self.assertEqual(leapyear(100), "Not Leap Year") 
+    def test3(self):
+        self.assertEqual(leapyear(2320349), "Not Leap Year")
 
 
 if __name__ == "__main__":
