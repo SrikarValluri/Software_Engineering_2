0622c87bdde07df4a57bc1afb8f25eee9807ec67
SrikarValluri <valluris@oregonstate.edu>
2021-03-07 23:01:59 -0800

    failed test 2 q1

--- hw7/q1_test.py
+++ hw7/q1_test.py
@@ -4,6 +4,8 @@ from q1 import fizzbuzz
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(fizzbuzz(1), "1")
+    def test2(self):
+        self.assertEqual(fizzbuzz(3), "Fizz")
 
 if __name__ == "__main__":
     unittest.main()
