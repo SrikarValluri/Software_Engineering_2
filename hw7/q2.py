1a85d2419f925bd815ef3f593a688d5fe779527c
SrikarValluri <valluris@oregonstate.edu>
2021-03-07 23:16:23 -0800

    q2 first test working

--- hw7/q2.py
+++ hw7/q2.py
@@ -1,2 +1,5 @@
-def leapyear():
-    pass
+def leapyear(x):
+    if(x % 4 == 0):
+        return("Leap Year")
+    else:
+        return("Not Leap Year")
--- hw7/q2_test.py
+++ hw7/q2_test.py
@@ -3,7 +3,7 @@ from q2 import leapyear
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        self.assertEqual(leapyear(2000), "True")
+        self.assertEqual(leapyear(2000), "Leap Year")
 
 if __name__ == "__main__":
     unittest.main()
