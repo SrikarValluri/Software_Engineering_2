b58a188b1c7db4cfc551c8db33bb9316fa63d413
SrikarValluri <valluris@oregonstate.edu>
2021-03-07 23:07:43 -0800

    Succesful test 4

--- hw7/q1.py
+++ hw7/q1.py
@@ -1,5 +1,8 @@
 def fizzbuzz(x):
-    if(x % 3 == 0):
+    if(x % 15 == 0):
+        print("FizzBuzz")
+        return("FizzBuzz")
+    elif(x % 3 == 0):
         print("Fizz")
         return("Fizz")
     elif(x % 5 == 0):
