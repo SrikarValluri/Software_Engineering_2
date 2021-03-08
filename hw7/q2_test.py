2c248f1069ecec873b9e17e1a68360b2e40f0765
SrikarValluri <valluris@oregonstate.edu>
2021-03-07 23:17:41 -0800

    q2 added second failed test case

--- hw7/q2_test.py
+++ hw7/q2_test.py
@@ -4,6 +4,9 @@ from q2 import leapyear
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(leapyear(2000), "Leap Year")
+    def test2(self):
+        self.assertEqual(leapyear(100), "Not Leap Year")
+
 
 if __name__ == "__main__":
     unittest.main()
