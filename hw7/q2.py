4c177b97d72bdb2a4c5191a87e3ad6a820b37281
SrikarValluri <valluris@oregonstate.edu>
2021-03-07 23:28:28 -0800

    q2 sucessful both test cases

--- hw7/q2.py
+++ hw7/q2.py
@@ -1,7 +1,10 @@
 def leapyear(x):
     if(x % 4 == 0):
         if(x % 100 == 0):
-            return("Not Leap Year")
+            if(x % 400 == 0):
+                return("Leap Year")
+            else:
+                return("Not Leap Year")
         else:
             return("Leap Year")
     else:
